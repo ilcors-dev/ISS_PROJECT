e8e5483be9006c65c84d2515f4bf8f707b724b93
vals <valentina.derespinis@gmail.com>
2024-07-23 19:25:36 +0200

    modificata gerarchia cartelle e iniziato a scrivere sprint0

--- project/sprint0/WasteIncineratorService/Arch_wis24.py
+++ project/sprint0/WasteIncineratorService/Arch_wis24.py
@@ -27,23 +27,4 @@ with Diagram('wis24Arch', show=False, outformat='png', graph_attr=graphattr) as 
 ### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
      with Cluster('ctxwis24', graph_attr=nodeattr):
           wis=Custom('wis','./qakicons/symActorSmall.png')
-          oprobot=Custom('oprobot','./qakicons/symActorSmall.png')
-          incinerator=Custom('incinerator','./qakicons/symActorSmall.png')
-          monitoringdeviceobserver=Custom('monitoringdeviceobserver','./qakicons/symActorSmall.png')
-          weighingdeviceobserver=Custom('weighingdeviceobserver','./qakicons/symActorSmall.png')
-          wastestoragemock=Custom('wastestoragemock','./qakicons/symActorSmall.png')
-          ashstoragemock=Custom('ashstoragemock','./qakicons/symActorSmall.png')
-     with Cluster('ctxbasicrobot', graph_attr=nodeattr):
-          basicrobot=Custom('basicrobot(ext)','./qakicons/externalQActor.png')
-     sys >> Edge( label='burning', **evattr, decorate='true', fontcolor='darkgreen') >> wis
-     sys >> Edge( label='finishedBurning', **evattr, decorate='true', fontcolor='darkgreen') >> wis
-     incinerator >> Edge( label='burning', **eventedgeattr, decorate='true', fontcolor='red') >> sys
-     incinerator >> Edge( label='finishedBurning', **eventedgeattr, decorate='true', fontcolor='red') >> sys
-     sys >> Edge( label='sonarData', **evattr, decorate='true', fontcolor='darkgreen') >> monitoringdeviceobserver
-     sys >> Edge( label='reports', **evattr, decorate='true', fontcolor='darkgreen') >> weighingdeviceobserver
-     oprobot >> Edge(color='magenta', style='solid', decorate='true', label='<engage &nbsp; >',  fontcolor='magenta') >> basicrobot
-     monitoringdeviceobserver >> Edge(color='blue', style='solid',  decorate='true', label='<ashMeasurement &nbsp; >',  fontcolor='blue') >> wis
-     oprobot >> Edge(color='blue', style='solid',  decorate='true', label='<inhome &nbsp; endburnindeposit &nbsp; endashdeposit &nbsp; >',  fontcolor='blue') >> wis
-     weighingdeviceobserver >> Edge(color='blue', style='solid',  decorate='true', label='<rpInWaste &nbsp; >',  fontcolor='blue') >> wis
-     wis >> Edge(color='blue', style='solid',  decorate='true', label='<getash &nbsp; getrp &nbsp; gohome &nbsp; >',  fontcolor='blue') >> oprobot
 diag
--- project/sprint0/WasteIncineratorService/wis24.pl
+++ project/sprint0/WasteIncineratorService/wis24.pl
@@ -1,39 +1,7 @@
 %====================================================================================
 % wis24 description   
 %====================================================================================
-request( engage, engage(OWNER,STEPTIME) ).
-dispatch( getrp, getrp(X,Y) ).
-dispatch( endburnindeposit, endburnindeposit(WEIGHRP) ).
-dispatch( gohome, gohome(X,Y) ).
-dispatch( inhome, inhome(STATUS) ).
-dispatch( getash, getash(X,Y) ).
-dispatch( endashdeposit, endashdeposit(WEIGHASH) ).
-dispatch( startIncinerator, startIncinerator(BTIME) ).
-event( sonarData, distance(D) ).
-dispatch( ashMeasurement, ashMeasurement(rpCount) ).
-event( burning, burning(START_TIME) ).
-event( finishedBurning, finishedBurning(TIME_ELAPSED) ).
-event( reports, reports(WEIGHRP) ).
-dispatch( rpInWaste, rpInWaste(RPCOUNT) ).
-dispatch( sonarstart, sonarstart(X) ).
-dispatch( sonarstop, sonarstop(X) ).
-event( sonardata, distance(D) ).
-dispatch( doread, doread(X) ).
 %====================================================================================
 context(ctxwis24, "localhost",  "TCP", "8120").
-context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
- qactor( basicrobot, ctxbasicrobot, "external").
-  qactor( wis, ctxwis24, "it.unibo.wis.Wis").
+ qactor( wis, ctxwis24, "it.unibo.wis.Wis").
  static(wis).
-  qactor( oprobot, ctxwis24, "it.unibo.oprobot.Oprobot").
- static(oprobot).
-  qactor( incinerator, ctxwis24, "it.unibo.incinerator.Incinerator").
- static(incinerator).
-  qactor( monitoringdeviceobserver, ctxwis24, "it.unibo.monitoringdeviceobserver.Monitoringdeviceobserver").
- static(monitoringdeviceobserver).
-  qactor( weighingdeviceobserver, ctxwis24, "it.unibo.weighingdeviceobserver.Weighingdeviceobserver").
- static(weighingdeviceobserver).
-  qactor( wastestoragemock, ctxwis24, "it.unibo.wastestoragemock.Wastestoragemock").
- static(wastestoragemock).
-  qactor( ashstoragemock, ctxwis24, "it.unibo.ashstoragemock.Ashstoragemock").
- static(ashstoragemock).
